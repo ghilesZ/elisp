4d9fe9282a70b3b6dcfb4b910af55f826f5be9d1
ziat <ghiles.ziat@gmail.com>
2018-06-22 11:41:55 +0200

    sig2fun ++ to handle tuple arguments

--- sig2fun.el
+++ sig2fun.el
@@ -1,3 +1,17 @@
+;; transforme a type to a skeleton to complete
+;; ex:
+;;   val min_binding_opt : 'a t -> (key * 'a) option
+;; becomes
+;;  let min_binding_opt (x1:'a t) : (key * 'a) option =
+;;    (* TODO: replace the "failwith" with your own code *)
+;;    failwith "function 'min_binding_opt' in file 'filename' not implemented"
+;;
+;;  WARNING: does not handle yet arrow type argument
+
+(setq counter 0)
+
+;; we specify in the failwith msg the name of the function and the file wich
+;; defines it
 (defun body (fname)
   (setq com " =\n(* TODO: replace the \"failwith\" with your own code *)\n")
   (concat com "failwith \"function '" fname "' in file '"
@@ -5,16 +19,27 @@
           "' not implemented\"\n"))
 
 (defun namegen()
-   (setq counter (+ counter 1))
-   (concat "x" (number-to-string counter)))
+  (setq counter (+ counter 1))
+  (concat "x" (number-to-string counter)))
 
+;; in case there are tuples, we explode them
+(defun handleatom(typ)
+  (let((nostar (replace-regexp-in-string "*" "," typ)))
+    (concat
+     "("
+     (replace-regexp-in-string  "['A-Za-z ]+" (lambda (x) (namegen)) nostar)
+     ":"
+     typ
+     ") "
+     )))
+
+;; we dont need to add an argument to the return type
 (defun typatoms2arg(list)
   (setq counter 0)
   (let((returntyp (car (last list)))
        (argtyp (butlast list)))
     (concat
-     (reduce 'concat
-             (mapcar (function (lambda (x) (concat "(" (namegen) ":" x ") "))) argtyp))
+     (reduce 'concat (mapcar 'handleatom argtyp))
      ": "
      returntyp)))
 
@@ -23,6 +48,7 @@
   (replace-regexp-in-string "\\(val \\).*\\'" "" name nil nil 1))
 
 (defun sig2fun(str)
+  (setq counter 0) ;; reset the name counter
   (let*
       ((rgxcolon (regexp-quote ":"))
        (rgxarrow (regexp-quote "->"))
@@ -30,10 +56,17 @@
        (name (car list))
        (type (cadr list))
        (typatoms (split-string type rgxarrow))
-       (trimed (mapcar (function (lambda (x) (string-trim x))) typatoms))
+       (trimed (mapcar 'string-trim typatoms))
        (name (string-trim (handlename name))))
     (concat "let " name " " (typatoms2arg trimed) (body name))))
 
+;; few examples:
+;; WORKING:
+;; (sig2fun "val min_binding_opt : 'a t -> (key * 'a) option")
+;; NOT WORKING:
+;; (sig2fun "val merge : (key -> 'a option -> 'b option -> 'c option) -> 'a t -> 'b t -> 'c t")
+
+;; integrating the function to buffers
 (defun sig2funregion()
   (interactive)
   (let*((sig (buffer-substring-no-properties (region-beginning) (region-end)))
