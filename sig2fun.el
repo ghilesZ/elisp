75f4f78f1038581d86019b609a016e61e12ae48c
ziat <ghiles.ziat@gmail.com>
2018-06-29 09:53:54 +0200

    arrow type begining

--- sig2fun.el
+++ sig2fun.el
@@ -1,38 +1,60 @@
-;; transforme a type to a skeleton to complete
+;; transforms a type to a skeleton to complete
 ;; ex:
 ;;   val min_binding_opt : 'a t -> (key * 'a) option
 ;; becomes
 ;;  let min_binding_opt (x1:'a t) : (key * 'a) option =
 ;;    (* TODO: replace the "failwith" with your own code *)
 ;;    failwith "function 'min_binding_opt' in file 'filename' not implemented"
-;;
+;;  WARNING: the produced code might be dummy
 ;;  WARNING: does not handle yet arrow type argument
 
 (setq counter 0)
 
+;; dummy body of the function
 ;; we specify in the failwith msg the name of the function and the file wich
 ;; defines it
 (defun body (fname)
   (setq com " =\n(* TODO: replace the \"failwith\" with your own code *)\n")
-  (concat com "failwith \"function '" fname "' in file '"
+  (concat com "failwith \"value '" fname "' in file '"
           (file-name-nondirectory (buffer-file-name))
           "' not implemented\"\n"))
 
-(defun namegen()
+;; generation of argument name according to the type
+(defun namegen (x)
   (setq counter (+ counter 1))
-  (concat "x" (number-to-string counter)))
+  (pcase x
+    (`"unit" "()")
+    (`"Format.formatter" "fmt")
+    ((guard (string-match-p (regexp-quote "->") x)) (concat "f" (number-to-string counter)))
+    (x
+     (save-match-data
+       (let* ((rgxspace (regexp-quote " "))
+              (l (split-string x rgxspace));; in case type take argument ('a list, int option ...)
+              (namelast (car (last l)));; we consider only the outer type constructor
+              (firstchar (substring namelast 0 1)))
+         (concat firstchar (number-to-string counter)))))))
+
+;; (namegen "int")
+;; (namegen "bool")
+;; (namegen "Format.formatter")
+;; (namegen "unit")
+;; (namegen "'a list")
+;; (namegen "'a list option")
+;; (namegen "'a -> 'b")
 
 ;; in case there are tuples, we explode them
 (defun handleatom(typ)
-  (let((typregexp "['A-Za-z ]+")
+  (let((typregexp "['A-Za-z .]+")
        (nostar (replace-regexp-in-string "*" "," typ)))
     (concat
      "("
-     (replace-regexp-in-string typregexp (lambda (x) (namegen)) nostar)
+     (replace-regexp-in-string typregexp 'namegen nostar)
      ":"
      typ
      ") "
      )))
+
+;; (handleatom "'a list")
 
 ;; we dont need to add an argument to the return type
 (defun typatoms2arg(list)
@@ -63,7 +85,7 @@
 
 ;; few examples:
 ;; WORKING:
-;; (sig2fun "val min_binding_opt : 'a t -> (key * 'a) option")
+;; (sig2fun "val min_binding_opt : int -> float -> 'a list -> bool -> (key * 'a) option")
 ;; NOT WORKING:
 ;; (sig2fun "val merge : (key -> 'a option -> 'b option -> 'c option) -> 'a t -> 'b t -> 'c t")
 
