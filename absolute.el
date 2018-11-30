dd556993f3af018a63f72f4d1dc828b4db8bbcf5
ghilesZ <ghiles.ziat@gmail.com>
2018-11-30 14:26:25 +0100

    pouet

--- absolute.el
+++ absolute.el
@@ -1,13 +1,12 @@
-(require 'generic-x) ;; we need this
+(require 'generic-x) ;; you will need this
 
-(define-generic-mode
-    'absolute-mode                           ;; name of the mode to create
-  '("/*" "*/")                               ;; comments start with '/*' and end with '*/'
-  '("init" "constraints"
-    "real" "int")                            ;; some keywords
+(define-generic-mode 'absolute-mode          ;; name of the mode to create
+  '("/*" "*/")                               ;; comments: '/* some comment */'
+  '("init" "constraints" "solutions"
+    "real" "int")                            ;; keywords
   '(("\\(?:cos\\|exp\\|s\\(?:in\\|qrt\\)\\)"
-     . 'font-lock-function-name-face))        ;; function name displayed differently
-  '("\\.abs$")                               ;; files for which to activate this mode
+     . 'font-lock-function-name-face))       ;; function name
+  '("\\.abs$")                               ;; Absolute problem description
   nil                                        ;; other functions to call
-  "A mode for AbSolute files"                ;; doc string for this mode
+  "A mode for AbSolute files"                ;; doc string
   )
